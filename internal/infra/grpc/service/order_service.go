bdece5ac816d6abdc950de063e8de49972878dd9
celiotk <celio.kanekiyo@hotmail.com>
2024-11-17 23:28:13 -0300

    ListOrders com GRPC

--- internal/infra/grpc/service/order_service.go
+++ internal/infra/grpc/service/order_service.go
@@ -10,11 +10,13 @@ import (
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrderUseCase   usecase.ListOrderUseCase
 }
 
-func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase, listOrderUseCase usecase.ListOrderUseCase) *OrderService {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
+		ListOrderUseCase:   listOrderUseCase,
 	}
 }
 
@@ -33,5 +35,24 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Price:      float32(output.Price),
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
+	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, in *pb.Blank) (*pb.Orders, error) {
+	orders, err := s.ListOrderUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+	var ordersResponse []*pb.Order
+	for _, order := range orders {
+		ordersResponse = append(ordersResponse, &pb.Order{
+			Id:         order.ID,
+			Price:      float32(order.Price),
+			Tax:        float32(order.Tax),
+			FinalPrice: float32(order.FinalPrice),
+		})
+	}
+	return &pb.Orders{
+		Orders: ordersResponse,
 	}, nil
 }
